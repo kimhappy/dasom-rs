410740f24b5f00b852669a5e1295b320b57f7c4a
POMMI3R <kimsvrkimsvr@gmail.com>
2021-02-09 16:38:05 +0900

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 #![allow(dead_code)]
 
+//! # dasom-rs
+//! A toy ray tracing engine based on [Ray Tracing In One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) in Rust.
+//!
+//!You can easily render images and add your own shapes or materials easily.
+//!
+//![Examples](https://github.com/POMMI3R/dasom-rs/tree/master/examples) will be very helpful.
+
 pub mod color;
 pub mod ray_tracer;
 pub mod ray;
